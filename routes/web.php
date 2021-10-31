65fb3067cd301435f85f9514503f1e4cb99ac454
jing yuan chen <3a732031@gm.student.ncut.edu>
2021-10-31 10:28:07 +0800

    2-1 Route跳轉路由

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ Route::get('/', function () {
 Route::get('/',function(){
     return 'welcome';
 });
+Route::get('r1',function (){
+    return redirect('r2');
+});
+Route::get('r2',function (){
+    return view('welcome');
+});
