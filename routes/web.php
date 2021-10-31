ae259ebbea091e2d6a216e5d4624a8a7b0007e97
jing yuan chen <3a732031@gm.student.ncut.edu>
2021-10-31 10:35:11 +0800

    2-2 練習 2：設定 Route 接收參數

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/',function(){
+/*Route::get('/',function(){
     return 'welcome';
-});
+});*/
 Route::get('r1',function (){
     return redirect('r2');
 });
 Route::get('r2',function (){
     return view('welcome');
+});
+Route::get('hello/{name}',function ($name){
+    return 'Hello, '.$name;
+});
+Route::get('hello/{name?}',function ($name='Everybody'){
+    return 'Hello, '.$name;
 });
